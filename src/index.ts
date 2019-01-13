fd2690b6da0c935d5152773bb0f5482dc88eedcc
davechai <davecqx@gmail.com>
2019-01-13 15:00:52 +0800

    create server

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,31 @@
 import { ApolloServer } from "apollo-server-express"
 import * as Express from "express"
+import "reflect-metadata"
+import { buildSchema, Resolver, Query } from "type-graphql"
+
+@Resolver()
+class HelloResolver {
+  @Query(() => String)
+  async hello() {
+    // fake async in this example
+    return "Hello world"
+  }
+}
+
+const main = async () => {
+  const schema = await buildSchema({
+    resolvers: [HelloResolver],
+  })
+
+  const apolloServer = new ApolloServer({ schema })
+
+  const app = Express()
+
+  apolloServer.applyMiddleware({ app })
+
+  app.listen(9527, () => {
+    console.log("server started on http://localhost:9527")
+  })
+}
+
+main()
