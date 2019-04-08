36a93c328a9baa903a1eadd247c51cd41dcbd255
davechai <davecqx@gmail.com>
2019-04-08 19:30:01 +0800

    add dependency

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,10 @@ import { buildSchema, formatArgumentValidationError } from "type-graphql"
 import { createConnection } from "typeorm"
 
 import { RegisterResolver } from "./modules/user/Register"
+import { redis } from "./redis"
+import cors from "cors"
+import connectRedis from "connect-redis"
+import session from "express-session"
 
 const main = async () => {
   await createConnection()
@@ -19,6 +23,30 @@ const main = async () => {
   })
 
   const app = Express()
+  const RedisStore = connectRedis(session)
+
+  app.use(
+    cors({
+      credentials: true,
+      origin: "http://localhost:3000"
+    })
+  )
+
+  app.use(session({
+    store: new RedisStore({
+      client: redis as any
+    }),
+    name: "qid",
+    secret: "qwertyuiop",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 1000 * 60 * 60 * 24 * 7 * 365 // 7 years
+    }
+  })
+  )
 
   apolloServer.applyMiddleware({ app })
 
