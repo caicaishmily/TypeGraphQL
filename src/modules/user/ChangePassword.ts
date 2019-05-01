2dc3944a0945d15295cf7a6102afce3301de4d62
davechai <davecqx@gmail.com>
2019-05-01 23:07:32 +0800

    add change password resolver

---
+++ src/modules/user/ChangePassword.ts
@@ -0,0 +1,30 @@
+import { Mutation, Arg } from "type-graphql"
+import { User } from "src/entity/User"
+import { v4 } from "uuid"
+import { redis } from "src/redis"
+import { sendMail } from "../utils/sendEmail"
+
+export class ChangePasswordResolver {
+
+  @Mutation(() => Boolean)
+  async changePassword(
+    @Arg("email") email: string
+  ): Promise<boolean> {
+    const user = await User.findOne({ where: { email }})
+
+    if(!user) {
+      return true
+    }
+    const token = v4()
+
+    await redis.set(token, user.id, "ex", 60 * 60 * 24)
+
+    await sendMail(
+      email,
+      `http://localhost:3000/user/confirm/${token}`
+    )
+
+    return true
+
+  }
+}
